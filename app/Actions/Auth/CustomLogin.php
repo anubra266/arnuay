efef9cb7e4faa808641796929ff6cbf99f7976c1
anubra266 <anubra266@gmail.com>
2021-02-10 12:36:16 +0100

    🛂 Login with both username and email

--- app/Actions/Auth/CustomLogin.php
+++ app/Actions/Auth/CustomLogin.php
@@ -2,15 +2,16 @@
 
 namespace App\Actions\Auth;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Laravel\Fortify\Fortify;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Fortify\Actions\EnsureLoginIsNotThrottled;
 use Laravel\Fortify\Actions\PrepareAuthenticatedSession;
 use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
-use Laravel\Fortify\Fortify;
-use Illuminate\Http\Request;
 
 class CustomLogin
 {
-
     public static function handle()
     {
         Fortify::authenticateThrough(function (Request $request) {
@@ -20,6 +21,18 @@ class CustomLogin
                 CustomAttemptToAuthenticate::class,
                 PrepareAuthenticatedSession::class,
             ]);
+        });
+
+        Fortify::authenticateUsing(function (Request $request) {
+            //* Accept either username or email
+            $user = User::where('email', $request->email)
+                ->orWhere('username', $request->email)
+                ->first();
+
+            if ($user &&
+                Hash::check($request->password, $user->password)) {
+                return $user;
+            }
         });
     }
 }
